cb5bf62848408a365e48083192eda4e939b5f6b3
StanislavSiteMaker1989 <51828845+StanislavSiteMaker1989@users.noreply.github.com>
2020-07-27 20:28:32 +0300

    Updates

---
+++ precache-manifest.a93b509bf333f70fdaabde87252a119a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1d21bc9ae2c36eb72618cf0c065ef1",
+    "url": "/ReactSocial/index.html"
+  },
+  {
+    "revision": "2586a27a995fa14ba0c0",
+    "url": "/ReactSocial/static/css/3.0dbcd7d3.chunk.css"
+  },
+  {
+    "revision": "f97116474b89857b9665",
+    "url": "/ReactSocial/static/css/4.8ae4ed97.chunk.css"
+  },
+  {
+    "revision": "8d91e46fa4b3ab929bce",
+    "url": "/ReactSocial/static/css/main.44d12d77.chunk.css"
+  },
+  {
+    "revision": "701ba54c7622484a072f",
+    "url": "/ReactSocial/static/js/2.b4ff5aa8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ReactSocial/static/js/2.b4ff5aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2586a27a995fa14ba0c0",
+    "url": "/ReactSocial/static/js/3.a472e272.chunk.js"
+  },
+  {
+    "revision": "f97116474b89857b9665",
+    "url": "/ReactSocial/static/js/4.e33eb600.chunk.js"
+  },
+  {
+    "revision": "8d91e46fa4b3ab929bce",
+    "url": "/ReactSocial/static/js/main.598749a5.chunk.js"
+  },
+  {
+    "revision": "88ddd73b0db3308b845e",
+    "url": "/ReactSocial/static/js/runtime-main.0767c22d.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/ReactSocial/static/media/loader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/ReactSocial/static/media/user.8b1dbb1f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactSocial.git/precache-manifest.12718e203b0fd98e0c8af03f0c8448f9.js"
+  "/ReactSocial/precache-manifest.a93b509bf333f70fdaabde87252a119a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactSocial.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactSocial/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
