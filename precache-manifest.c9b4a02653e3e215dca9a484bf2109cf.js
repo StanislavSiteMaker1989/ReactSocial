e811f3e99834141ff7ba78bdfec9d36a6e57148e
StanislavSiteMaker1989 <51828845+StanislavSiteMaker1989@users.noreply.github.com>
2020-07-27 20:05:07 +0300

    Updates

---
+++ precache-manifest.c9b4a02653e3e215dca9a484bf2109cf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "764c79f268c04a33e4b17fad99fd4570",
+    "url": "/ReactSocial.git/index.html"
+  },
+  {
+    "revision": "2586a27a995fa14ba0c0",
+    "url": "/ReactSocial.git/static/css/3.0dbcd7d3.chunk.css"
+  },
+  {
+    "revision": "f97116474b89857b9665",
+    "url": "/ReactSocial.git/static/css/4.8ae4ed97.chunk.css"
+  },
+  {
+    "revision": "56e68b712a4cfde4a3a0",
+    "url": "/ReactSocial.git/static/css/main.44d12d77.chunk.css"
+  },
+  {
+    "revision": "701ba54c7622484a072f",
+    "url": "/ReactSocial.git/static/js/2.b4ff5aa8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ReactSocial.git/static/js/2.b4ff5aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2586a27a995fa14ba0c0",
+    "url": "/ReactSocial.git/static/js/3.a472e272.chunk.js"
+  },
+  {
+    "revision": "f97116474b89857b9665",
+    "url": "/ReactSocial.git/static/js/4.e33eb600.chunk.js"
+  },
+  {
+    "revision": "56e68b712a4cfde4a3a0",
+    "url": "/ReactSocial.git/static/js/main.a26d12dc.chunk.js"
+  },
+  {
+    "revision": "4d132fa01de5bf6d7cd9",
+    "url": "/ReactSocial.git/static/js/runtime-main.4ce131e0.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/ReactSocial.git/static/media/loader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/ReactSocial.git/static/media/user.8b1dbb1f.png"
+  }
+]);
